cf9d003c059aa8c8ff8aa196543938af53318145
Panto1212 <patienceankundat@gmail.com>
2018-06-24 22:19:26 +0300

    add side navigation operation

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "100%";
+    document.getElementById("navigation").style.width = "100%";
 }
 
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("navigation").style.width = "0";
 }
