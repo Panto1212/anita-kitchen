8c76358590ad22007abe0ecaabc370e2d5399b48
Panto1212 <patienceankundat@gmail.com>
2018-06-25 11:42:45 +0300

    align row 8

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/*row1*/
 function openNav() {
     document.getElementById("navigation").style.width = "100%";
 }
@@ -5,3 +6,10 @@ function openNav() {
 function closeNav() {
     document.getElementById("navigation").style.width = "0";
 }
+/*row7*/
+
+  $("#blanks form").submit(function(event) {
+    var blanks = ["email"];
+    alert("blanks"+"has been successfully added to our emailing list. Thank you"+".");
+    event.preventDefault();
+  });
